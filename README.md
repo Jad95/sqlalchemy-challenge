94a54c0003f4e943d4f81cb9759e58c91e5f59c6
Jad95 <amjadabdullah7667@gmail.com>
2023-07-10 21:34:46 -0400

    app.py,readme updated

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # sqlalchemy-challenge
+
+# Climate Analysis and Flask API
+
+This project involves analyzing climate data from the island of Hawaii and creating a Flask API to provide access to the data through various routes.
+
+## Dataset
+
+The dataset used for the analysis is stored in a SQLite database file named "hawaii.sqlite" located in the "Resources" folder. It contains two tables: "Measurement" and "Station".
+
+## Jupyter Notebook Code
+
+The code in the Jupyter Notebook file "climate_starter.ipynb" performs the following tasks:
+
+1. Importing the necessary dependencies and setting up the database connection.
+
+2. Retrieving the most recent date in the dataset and calculating the date one year prior to that.
+
+3. Querying the precipitation data for the last 12 months and plotting it as a line graph.
+
+4. Calculating the summary statistics for the precipitation data.
+
+5. Finding the total number of stations in the dataset.
+
+6. Determining the most active stations and their row counts in descending order.
+
+7. Calculating the lowest, highest, and average temperature for the most active station.
+
+8. Querying the temperature observations for the last 12 months of the most active station and plotting it as a histogram.
+
+9. Closing the database session.
+
+## Flask API
+
+The Flask API is implemented in the "app.py" file and provides the following routes:
+
+1. `/` (Homepage): Displays a welcome message and lists all available API routes.
+
+2. `/api/v1.0/precipitation`: Retrieves the last 12 months of precipitation data and returns it as a JSON object with date as the key and prcp as the value.
+
+3. `/api/v1.0/stations`: Retrieves the list of stations from the dataset and returns it as a JSON list.
+
+4. `/api/v1.0/tobs`: Retrieves the temperature observations for the last 12 months of the most active station and returns them as a JSON list.
+
+5. `/api/v1.0/<start>`: Calculates the minimum, maximum, and average temperature for all dates greater than or equal to the specified start date and returns them as a JSON object.
+
+6. `/api/v1.0/<start>/<end>`: Calculates the minimum, maximum, and average temperature for the specified date range (inclusive) and returns them as a JSON object.
+
+# Refrences
+https://github.com/Baylex/surfs_up/blob/main
+https://flask.palletsprojects.com/
+https://docs.sqlalchemy.org/
+https://www.sqlite.org/docs.html
+
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,86 @@
-# Import the dependencies.
+from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+# Create engine and reflect the database
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
+# Reflect database into classes
+Base = automap_base()
 
-#################################################
-# Database Setup
-#################################################
+# Reflect tables
+Base.prepare(engine, reflect=True)
 
+# Save references to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
-# reflect an existing database into a new model
+# Create session
+session = Session(engine)
 
-# reflect the tables
+# Flask setup
+app = Flask(__name__)
 
+# Flask routes
+@app.route("/")
+def home():
+    """List all available routes."""
+    return (
+        f"Welcome to the Climate API!<br/><br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;"
+    )
 
-# Save references to each table
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
 
+@app.route("/api/v1.0/stations")
 
-# Create our session (link) from Python to the DB
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
 
+@app.route("/api/v1.0/tobs")
 
-#################################################
-# Flask Setup
-#################################################
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
 
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
 
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
 
-
-#################################################
-# Flask Routes
-#################################################
+if __name__ == "__main__":
+    app.run(debug=True)
